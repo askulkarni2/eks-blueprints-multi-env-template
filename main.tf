e25778fad4fa4c8d172386a3ef773bbccc57e66b
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-06-14 01:14:13 -0700

    change region to us-east-2

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ data "aws_eks_cluster" "eks_cluster" {
 
 locals {
   name   = basename(path.cwd)
-  region = "us-east-1"
+  region = "us-east-2"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
